f334a1117329584f999e4b4c35c15e68d5a7620e
Charlie <edie111370257@gmail.com>
2025-02-22 15:04:10 +0000

    20250222

--- LeetcodePython/136. Single Number.py
+++ LeetcodePython/136. Single Number.py
@@ -1,19 +1,19 @@
 class Solution:
-    def romanToInt(self, s: str) -> int:
-        roman_to_int = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        total = 0
-        prev_value = 0
+    def singleNumber(self, nums: list[int]) -> int:
+        checkList = {}
 
-        for char in reversed(s):
-            value = roman_to_int[char]
-            if prev_value > value:
-                total -= value
+        for i in nums:
+            if i not in checkList:
+                checkList[i] = 1
             else:
-                total += value
-            prev_value = value
-        return total
+                checkList[i] = checkList[i] + 1
+
+        for i in checkList:
+            if checkList[i] == 1:
+                return i
+
 
 if __name__ == '__main__':
     solution = Solution()
-    result = solution.romanToInt("III")
+    result = solution.singleNumber([4,1,2,1,2])
     print(result)
