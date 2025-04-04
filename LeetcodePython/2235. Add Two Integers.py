f9327721abd347a13ba65385fe2ba602241c7680
Charlie <edie111370257@gmail.com>
2025-04-04 16:28:09 +0100

    20250404

---
+++ LeetcodePython/2235. Add Two Integers.py
@@ -0,0 +1,10 @@
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+        output = num1 + num2
+        return output
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    result = solution.sum(5,12)
+    print(result)
